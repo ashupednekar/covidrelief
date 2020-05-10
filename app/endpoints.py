65fbdaa48d04ff97808af69be49a2a951e3014c1
ashupednekar <ashutosh.pednekar@bankbuddy.ai>
2020-05-10 16:08:26 +0530

    added actor field to entities

--- app/endpoints.py
+++ app/endpoints.py
@@ -29,6 +29,8 @@ class EntryView(
         serializer.is_valid(raise_exception=True)
         serializer.validated_data['actor'] = request.user
         self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def get(self, request, mobile=None):
         if mobile:
