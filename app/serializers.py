0317373575aa57b037b2720adb956ec73a51d977
ashupednekar <ashutosh.pednekar@bankbuddy.ai>
2020-05-10 18:34:12 +0530

    datatables debug :) exp

--- app/serializers.py
+++ app/serializers.py
@@ -13,6 +13,7 @@ class EntrySerializer(ModelSerializer):
     class Meta:
         model = Entries
         fields = (
+            'ward',
             'actor',
             'name',
             'mobile',
