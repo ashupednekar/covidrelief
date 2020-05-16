c69965e14d59ed87a06140bf887794d92e74cf20
Prateek <prateek@couture.ai>
2020-05-16 23:01:16 +0530

    no initial centers

--- app/extras.py
+++ app/extras.py
@@ -1,13 +1,14 @@
 from app.models import Centers
 
 def initialize_centers():
-    Centers.objects.update_or_create(center_name='Jankipuram')
-    Centers.objects.update_or_create(center_name='Faizullaganj 1')
-    Centers.objects.update_or_create(center_name='Faizullaganj 2')
-    Centers.objects.update_or_create(center_name='Aliganj')
-    Centers.objects.update_or_create(center_name='Triveni Nagar')
-    Centers.objects.update_or_create(center_name='Daliganj')
-    Centers.objects.update_or_create(center_name='Khadra')
-    Centers.objects.update_or_create(center_name='Hussainabad')
-    Centers.objects.update_or_create(center_name='Daulatganj')
-    Centers.objects.update_or_create(center_name='Thakurganj')
+    pass
+    # Centers.objects.update_or_create(center_name='Jankipuram')
+    # Centers.objects.update_or_create(center_name='Faizullaganj 1')
+    # Centers.objects.update_or_create(center_name='Faizullaganj 2')
+    # Centers.objects.update_or_create(center_name='Aliganj')
+    # Centers.objects.update_or_create(center_name='Triveni Nagar')
+    # Centers.objects.update_or_create(center_name='Daliganj')
+    # Centers.objects.update_or_create(center_name='Khadra')
+    # Centers.objects.update_or_create(center_name='Hussainabad')
+    # Centers.objects.update_or_create(center_name='Daulatganj')
+    # Centers.objects.update_or_create(center_name='Thakurganj')
